4839fcc9556bbfc4266bf50e4ccc4ece41486eab
wildspider57389928 <bardiamehrju@gmail.com>
2025-11-24 21:32:23 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ import requests
 
 TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
-PHOTO_PATH = "logo_mbb.jpg"  # مسیر عکس
-CAPTION = "پیام انگیزشی امروز 🌟"
+VIDEO_PATH = "VID.mp4"  # مسیر ویدئو
+CAPTION = "ویدئوی انگیزشی امروز 🎬"
 
-url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto"
-with open(PHOTO_PATH, "rb") as photo:
-    requests.post(url, data={"chat_id": CHANNEL_ID, "caption": CAPTION}, files={"photo": photo})
+url = f"https://api.telegram.org/bot{TOKEN}/sendVideo"
+with open(VIDEO_PATH, "rb") as video:
+    requests.post(url, data={"chat_id": CHANNEL_ID, "caption": CAPTION}, files={"video": video})
