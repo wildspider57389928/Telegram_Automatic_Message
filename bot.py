a88b39d7354a673358c091ef93bf63bb15189a31
wildspider57389928 <bardiamehrju@gmail.com>
2026-06-02 12:21:28 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -71,7 +71,7 @@ def analyze_news_with_gemini(description):
 {description}
 """
     try:
-        response = client.models.generate_content(model="gemini-2.5-flash",contents=prompt)
+        response = client.models.generate_content(model="gemini-3.1-flash-lite",contents=prompt)
         return response.text.strip()
     except Exception as e:
         return f"خطا در تحلیل خبر:{e}"
